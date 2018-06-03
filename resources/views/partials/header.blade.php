ed3305e639235fe7f2273ca79e066c1549ecaac0
Tan Dai <tandai96@gmail.com>
2018-06-04 00:06:47 +0700

    add menu song upload

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -17,6 +17,11 @@ use Illuminate\Support\Facades\Auth;
     <!-- Collect the nav links, forms, and other content for toggling -->
     <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
       <ul class="nav navbar-nav navbar-right nav__bar_right">
+        @if (Auth:: check())
+          @if ( Auth::user()->name == 'admin') 
+          <li><a href="{{ route('Uploadsong') }}">Upload Song</a></li>
+          @endif
+        @endif
         @if (!Auth:: check())
         <li><a href="#" data-toggle="modal" data-target="#login_Modal">Login</a></li>
         <li><a href="#" data-toggle="modal" data-target="#signup_Modal">Sign up</a></li>
